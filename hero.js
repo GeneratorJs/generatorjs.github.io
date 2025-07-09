3f2aab54955239c579d3e241e751429b43a67755
Dr. Prateek Raj Gautam <prateekrajgautam@gmail.com>
2025-07-09 13:55:53 +0530

    Update hero.js to include SevenJS

--- hero.js
+++ hero.js
@@ -2,9 +2,11 @@
 function LoadHero(){
     append(appmain, gen("section", "hero", gen("h1", "", site.title), 'section container'), 'over')
 
-append(hero,
-    [
-        gen(p, '', 'Website frontend design with less typing, less editing to HTML CSS <br />Few functions to learn or precisely <b>three</b> functions to be learned</b>.'),
+    append(hero,
+    [        
+        gen(p,"","Also knows as")
+        gen(h2,"","SevenJS : 7 function frontend library")
+        gen(p, '', 'Frontend design with less typing, less editing to HTML CSS <br />With only <b>Seven</b> functions</b>.'),
         gen(span, 'installButton', 'Install', 'button')
     ]
 )
